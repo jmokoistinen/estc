dd10526beec0f5c064f093bc04bc7dc472e7585d
Ville Vaara <villepvaara@gmail.com>
2016-10-17 14:44:13 +0300

    further testing

--- shinytest2/app.R
+++ shinytest2/app.R
@@ -18,18 +18,27 @@ ui <- shinyUI(fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("range.years",
+         sliderInput("range_years",
                      "Years:",
                      min = 1450,
                      max = 1850,
-                     value = c(1600, 1750))
-
+                     value = c(1600, 1750)),
+         textInput("keyword_search",
+                   "Keyword:",
+                   value = "garden",
+                   width = NULL,
+                   placeholder = "keyword"),
+         submitButton(text = "Apply Changes",
+                      icon = icon("blind"),
+                      width = NULL)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         textOutput("text1"),
-         plotOutput("yearPlot")
+         textOutput("text_debug"),
+         plotOutput("year_plot"),
+         plotOutput("hits_plot"),
+         plotOutput("hits_averages_plot")
       )
    )
 ))
@@ -38,32 +47,63 @@ ui <- shinyUI(fluidPage(
 
 # server data etc
 source("helper_shinytesting.R")
+catalog_data <- readRDS("shinytest2.Rds")
+publications_yearly <- get_publications_yearly(catalog_data)
 library(ggplot2)
 
 server <- shinyServer(function(input, output) {
+  
+  output$text_debug <- renderText({
+    sprintf("DEBUG: years low: %s, years high: %s, keyword: %s",
+           input$range_years[1],
+           input$range_years[2],
+           input$keyword_search)
+  })
    
-   output$text1 <- renderText({
-     sprintf("years low, %s years high %s",
-             input$range.years[1],
-             input$range.years[2])
-   })
-   
-   output$yearPlot <- renderPlot({
-     year.min <- input$range.years[1]
-     year.max <- input$range.years[2]
-     years  <- catalog.estc.amount.by.year$years
-     titles <- catalog.estc.amount.by.year$titles
-     year.min.index <- which(years == year.min)[[1]]
-     year.max.index <- which(years == year.max)[[1]]
-     years.subset  <- years[year.min.index:year.max.index]
-     titles.subset <- titles[year.min.index:year.max.index]
-     
-     estc.subset <- data.frame(years = years.subset,
-                               titles = titles.subset)
-     
-     qplot(years.subset, titles.subset, data = estc.subset)
-   })
-   
+  output$year_plot <- renderPlot({
+    year_min <- input$range_years[1]
+    year_max <- input$range_years[2]
+    years  <- publications_yearly$years
+    titles <- publications_yearly$titles
+    year_min_index <- which(years == year_min)[[1]]
+    year_max_index <- which(years == year_max)[[1]]
+    years_subset  <- years[year_min_index:year_max_index]
+    titles_subset <- titles[year_min_index:year_max_index]
+    estc_subset <- data.frame(years = years_subset,
+                             titles = titles_subset)
+    qplot(years_subset, titles_subset, data = estc_subset,
+          geom = c("point", "smooth"))
+  })
+
+  output$hits_plot <- renderPlot({
+    input_years <- c(input$range_years[1], input$range_years[2])
+    keyword <- input$keyword_search
+    yearly_hits <- get_hits_yearly(catalog_data, input_years, keyword)   
+    years <- yearly_hits$years
+    hits  <- yearly_hits$hits
+    qplot(years, hits, data = yearly_hits,
+          geom = c("point", "smooth"))
+  })
+  
+  output$hits_averages_plot <- renderPlot({
+
+    input_years <- c(input$range_years[1], input$range_years[2])
+    publications_yearly_subset <-
+      subset(publications_yearly, years >= input_years[1] &
+             years <= input_years[2])
+    keyword <- input$keyword_search
+    yearly_hits <- get_hits_yearly(catalog_data, input_years, keyword)   
+    yearly_hits["total_publications"] <- publications_yearly_subset[2]
+    yearly_hits["averages"] <-
+      yearly_hits["hits"] / yearly_hits["total_publications"]
+    years <- yearly_hits$years
+    averages  <- yearly_hits$averages
+    qplot(years, averages, data = yearly_hits,
+          geom = c("point", "smooth"))
+  })
+  
+  
+       
 })
 
 # Run the application 
--- shinytest2/helper_shinytesting.R
+++ shinytest2/helper_shinytesting.R
@@ -1,18 +1,45 @@
 
-catalog.estc <- readRDS("shinytest2.Rds")
-catalog.estc.years <- catalog.estc$publication_year
-# rm(catalog.estc)
+get_publications_yearly <- function(catalog_data) {
+  
+  catalog_years <- catalog_data$publication_year
+  years_range <- 1450:1850
+  years_titles <- vector("integer", length(years_range))
+  
+  i <- 1
+  for (year in years_range) {
+    years_titles[i] <- length(which(catalog_years == year))
+    i <- i + 1 
+  }
+  
+  publications_yearly <- data.frame(years = years_range,
+                                            titles = years_titles)
 
-years.range <- 1450:1850
-years.titles <- vector("integer", length(years.range))
-
-i <- 1
-for (year in years.range) {
-  # print(year)
-  # print(length(which(catalog.estc.years == year)))
-  years.titles[i] <- length(which(catalog.estc.years == year))
-  i <- i + 1 
+  return(publications_yearly)
 }
 
-catalog.estc.amount.by.year <- data.frame(years = years.range,
-                                          titles = years.titles)
+
+get_hits_yearly <- function(catalog_data, years, keyword) {
+
+  years_range <- years[1]:years[2]
+  yearly_hits <- vector("integer", length(years_range))
+  keyword <- tolower(keyword)
+    
+  items_considered <- subset(catalog_data,
+                             publication_year >= years[1] &
+                               publication_year <= years[2])
+
+  items_hit <- subset(items_considered,
+                      grepl(keyword,
+                            tolower(items_considered$title)))
+
+  i <- 1
+  for (year in years_range) {
+    yearly_hits[i] <- length(which(items_hit$publication_year == year))
+    i <- i + 1 
+  }
+
+  keyword_hits_yearly <- data.frame(years = years_range,
+                                    hits = yearly_hits)
+  
+  return(keyword_hits_yearly)
+}
